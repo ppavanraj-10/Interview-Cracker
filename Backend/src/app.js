86bc76f5a68d101e7b4af4867676e9b1ec57f18e
ppavanraj-10 <pavanrajpatha46@gmail.com>
2026-05-10 14:35:27 +0530

    resolved CORS error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(cors({ origin: "*" }))
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
